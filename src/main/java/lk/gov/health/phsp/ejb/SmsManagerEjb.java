f76ded8209b1b8bf9c64e24e41669c3720fc2209
buddhika <buddhika@moh-nchis>
2021-09-27 20:34:55 +0530

    Fixed Admin Error in Formsets

--- src/main/java/lk/gov/health/phsp/ejb/SmsManagerEjb.java
+++ src/main/java/lk/gov/health/phsp/ejb/SmsManagerEjb.java
@@ -1,4 +1,5 @@
 package lk.gov.health.phsp.ejb;
+
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStream;
@@ -6,6 +7,7 @@ import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -30,8 +32,10 @@ import lk.gov.health.phsp.facade.SmsFacade;
  */
 @Stateless
 public class SmsManagerEjb {
+
     @EJB
     SmsFacade smsFacade;
+
     @SuppressWarnings("unused")
     @Schedule(second = "19", minute = "*", hour = "*", persistent = false)
     public void myTimer() {
@@ -44,7 +48,7 @@ public class SmsManagerEjb {
         Calendar c = Calendar.getInstance();
         c.set(Calendar.HOUR_OF_DAY, 0);
         m.put("d", c.getTime());
-        List<Sms> smses = getSmsFacade().findByJpql(j,m,TemporalType.DATE);
+        List<Sms> smses = getSmsFacade().findByJpql(j, m, TemporalType.DATE);
         for (Sms e : smses) {
             e.setSentSuccessfully(Boolean.TRUE);
             e.setAwaitingSending(false);
@@ -57,6 +61,54 @@ public class SmsManagerEjb {
     }
 
     public String executePost(String targetURL, Map<String, String> parameters) {
+        System.out.println("executePost");
+        System.out.println("parameters = " + parameters);
+        System.out.println("targetURL = " + targetURL);
+
+        if (parameters != null && !parameters.isEmpty()) {
+            targetURL += "?";
+        }
+        Set s = parameters.entrySet();
+        Iterator it = s.iterator();
+        while (it.hasNext()) {
+            Map.Entry m = (Map.Entry) it.next();
+            String pVal;
+            System.out.println("m.getValue() = " + m.getValue());
+            try {
+                pVal = java.net.URLEncoder.encode(m.getValue().toString(), "UTF-8");
+            } catch (UnsupportedEncodingException ex) {
+                pVal = "";
+                Logger.getLogger(SmsManagerEjb.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            String pPara = (String) m.getKey();
+            System.out.println("pPara = " + pPara);
+            targetURL += pPara + "=" + pVal.toString() + "&";
+        }
+        System.out.println("targetURL = " + targetURL);
+        if (parameters != null && !parameters.isEmpty()) {
+            targetURL += "last=true";
+        }
+        String inputLine = "";
+        try {
+            URL hh = new URL(targetURL);
+            URLConnection connection = hh.openConnection();
+            String redirect = connection.getHeaderField("Location");
+            if (redirect != null) {
+                connection = new URL(redirect).openConnection();
+            }
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            System.out.println();
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println(inputLine);
+            }
+        } catch (Exception e) {
+            System.out.println("e = " + e);
+        }
+        return inputLine;
+    }
+
+    public String executePostOld(String targetURL, Map<String, String> parameters) {
         System.out.println("executePost");
         System.out.println("parameters = " + parameters);
         System.out.println("targetURL = " + targetURL);
@@ -122,7 +174,7 @@ public class SmsManagerEjb {
         m.put("number", number);
         m.put("message", message);
         m.put("key", decKey);
-        String res = executePost("http://chims.health.gov.lk/sms-mw", m);
+        String res = executePost("https://hims.health.gov.lk/sms-mw", m);
         System.out.println("res = " + res);
         if (res == null) {
             return false;
